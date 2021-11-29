7ec67f233e5337d05dad2188c4ba576bb1e266c2
Vancios86 <albuion86@gmail.com>
2021-11-29 09:23:12 +0000

    fix the upstream dependency conflict

--- smart-app-frontend/src/App.js
+++ smart-app-frontend/src/App.js
@@ -2,7 +2,7 @@ import Navigation from "./components/navigation/Navigation";
 import Logo from "./components/logo/Logo";
 import ImageLinkForm from "./components/imageLinkForm/ImageLinkForm";
 import PredictionsCount from "./components/predictionsCount/PredictionsCount";
-import TheParticles from "./Particles";
+import TheParticles from "./components/particles/Particles";
 
 function App() {
   return (
